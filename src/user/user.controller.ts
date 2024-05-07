2c05f586c90f9f6501e57365838b2d7c72cadb4a
Malik <malik-thalhaoui@hotmail.fr>
2024-05-07 20:14:19 +0200

    Controle securité login

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,11 @@
-import { Controller, Get, Render, Post, Body } from '@nestjs/common';
+import { Session, Controller, Get, Render, Post, Body, Redirect , UseInterceptors, ClassSerializerInterceptor } from '@nestjs/common';
+import { SignupDto } from './dto/SignupDto';
+import { LoginDto } from './dto/LoginDto';
+import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
+    constructor(private readonly userService : UserService){}
 
     @Get('/signup')
     @Render('user/signup')
@@ -12,8 +16,17 @@ export class UserController {
     getLogin(){}
 
     @Post('/signup')
-    postsignup(@Body() body : any){
-        return body
+    @Redirect('user/login')
+    async postSignup(@Body() body : SignupDto){
+        return {message : await this.userService.postSignup(body)}
     }
     
+    @UseInterceptors(ClassSerializerInterceptor)
+    @Post('/login')
+    async postlogin(@Body() body: LoginDto, @Session() session : Record<string, any>){
+        const user = await this.userService.postLogin(body)
+        session.user = user
+        session.connected = true
+        return session
+    }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import { SignupDto } from './dto/SignupDTO';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+import* as bcrypt from "bcrypt"
+import { LoginDto } from './dto/LoginDto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(
+        @InjectRepository(User)
+        private readonly usersrepository: Repository<User>,
+        ) {}
+        async postSignup(body: SignupDto): Promise<string> {
+        try{
+            const {password} = body
+            const hash = await bcrypt.hashSync(password,10)
+            const user = this.usersrepository.create({...body, password : hash})
+            await this.usersrepository.save(user)
+            return "Utilisateur crée !"
+        } catch(error){
+            throw new ConflictException(error.message);
+        }
+    }
+    async postLogin(body: LoginDto) {
+        const {password, email} = body
+        const user = await this.usersrepository.findOne({where : {email : email}})
+        if(!user )throw new NotFoundException("Utilisateur Introuvable")
+        const match = await bcrypt.compare(password, user.password)
+        if(!match) throw new UnauthorizedException("Mot de passe incorrect")
+        return user
+    }
+}
